696c1d8c2e6c08c833eaf2969ad068cff854eaf3
heyhi <heyhi@heyhis-MacBook-Pro.local>
2025-12-05 10:42:47 +0900

    feat: create nest backend

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+// src/app.module.ts
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ComponentsModule } from './components/components.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        autoLoadEntities: true,
+        synchronize: true, // ❗ only for local dev
+      }),
+    }),
+    ComponentsModule,
+  ],
+})
+export class AppModule {}
